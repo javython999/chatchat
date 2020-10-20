2b139993c7540e9ea76c056586a53184524a7b99
jjy <infinity2544@gamil.com>
2020-10-20 19:18:39 +0900

    Updates

---
+++ precache-manifest.9946a3234a47064c8201c161087a7ab1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb02031633b2a4e41bd881e543188844",
+    "url": "/chatchat/index.html"
+  },
+  {
+    "revision": "fb07607e94600488b26a",
+    "url": "/chatchat/static/css/main.9da127bd.chunk.css"
+  },
+  {
+    "revision": "8dafdec855c286101bc9",
+    "url": "/chatchat/static/js/2.214d72ad.chunk.js"
+  },
+  {
+    "revision": "22c95ed68071a711843b9981e2bca1af",
+    "url": "/chatchat/static/js/2.214d72ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb07607e94600488b26a",
+    "url": "/chatchat/static/js/main.5a867e01.chunk.js"
+  },
+  {
+    "revision": "0c7ad7b1f84a7e617d37",
+    "url": "/chatchat/static/js/runtime-main.2acc340a.js"
+  }
+]);
